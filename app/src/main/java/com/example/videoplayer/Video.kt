7812707f02a6565ab0115ad1d9bf636f009031cc
João Pedro <jplimao12@gmail.com>
2021-10-15 07:56:28 -0300

    Create list of videos

---
+++ app/src/main/java/com/example/videoplayer/Video.kt
@@ -0,0 +1,7 @@
+package com.example.videoplayer
+
+data class Video (
+    val title: String,
+    val author: String,
+    val url: String,
+    )
--- app/src/main/java/com/example/videoplayer/VideoListFragment.kt
+++ app/src/main/java/com/example/videoplayer/VideoListFragment.kt
@@ -4,55 +4,79 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
-import androidx.compose.material.Scaffold
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.*
 import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 
 class VideoListFragment : Fragment() {
+
+    private lateinit var navController: NavController
+    private lateinit var repository: VideoRepository
+
+    @ExperimentalMaterialApi
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val navController = findNavController()
+        // Change this to dependency injection using ViewModel
+        repository = VideoRepository()
+        navController =  findNavController();
         return ComposeView(requireContext()).apply {
             setContent {
-                Page(navController = navController)
+                Page()
             }
+        }
+    }
+
+    @ExperimentalMaterialApi
+    @Composable
+    fun Page() {
+        val videos = repository.getVideos()
+        Scaffold (
+            topBar = { TopAppBar(title = { Text("Video Fragment") }) }
+        ){
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                VideoList(videoList = videos)
+            }
+        }
+    }
+
+    @ExperimentalMaterialApi
+    @Composable
+    fun VideoList(
+        videoList: List<Video>
+    ) {
+        LazyColumn {
+            items(videoList.size) { data ->
+                VideoItem(video = videoList[data])
+            }
+        }
+    }
+
+    @ExperimentalMaterialApi
+    @Composable
+    fun VideoItem(video: Video) {
+        Card(
+            onClick = { navController.navigate(R.id.viewVideoPlayer) },
+            modifier = Modifier.padding(16.dp),
+            elevation = 4.dp
+        ){
+            Text(text = video.title)
         }
     }
 }
 
-@Composable
-fun Page(navController: NavController) {
-    Scaffold (
-        topBar = { TopAppBar(title = { Text("Video Fragment") }) }
-    ){
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(text = "Video Fragment")
-            Button(
-                onClick = {
-                    navController.navigate(R.id.viewVideoPlayer)
-                },
-
-                ){
-                Text (text = "To new Fragment")
-            }
-        }
-    }
-}
