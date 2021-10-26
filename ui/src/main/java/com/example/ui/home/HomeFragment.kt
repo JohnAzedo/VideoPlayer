7a9dc2609d71fa95dd33fa5ccd4e12b85622f77d
João Pedro Limão <jplimao12@gmail.com>
2021-10-26 09:34:51 -0300

    Show Item in fragment

--- ui/src/main/java/com/example/ui/home/HomeFragment.kt
+++ ui/src/main/java/com/example/ui/home/HomeFragment.kt
@@ -5,11 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.ui.databinding.HomeFragmentBinding
+import com.example.ui.home.HomeViewModelFactory.make
 
 class HomeFragment: Fragment() {
 
     private var bind: HomeFragmentBinding? = null
+    private val viewModel: HomeViewModel by lazy { make() }
+    private lateinit var trailAdapter: TrailAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -17,7 +21,25 @@ class HomeFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         bind = HomeFragmentBinding.inflate(inflater, container, false)
+        trailAdapter = TrailAdapter()
+        viewModel.getTrails()
         return bind?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        viewModel.trails.observe(viewLifecycleOwner) {
+            trailAdapter.submitList(it)
+        }
+
+        bind?.let {
+            it.rvTrail.apply { 
+                layoutManager = LinearLayoutManager(activity)
+                adapter = trailAdapter
+            }
+        }
+
+        super.onViewCreated(view, savedInstanceState)
     }
 
     override fun onDestroy() {
--- ui/src/main/java/com/example/ui/home/HomeViewModelFactory.kt
+++ ui/src/main/java/com/example/ui/home/HomeViewModelFactory.kt
@@ -6,14 +6,16 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.data.config.RetrofitClient
 import com.example.data.repositories.MovieRepositoryImpl
 import com.example.data.services.MovieService
+import com.example.domain.usecases.TrailUseCaseImpl
 
 object HomeViewModelFactory {
 
     private class Factory: ViewModelProvider.Factory {
         override fun <T: ViewModel?> create(modelClass: Class<T>): T {
             val service =  RetrofitClient.buildService(MovieService::class.java)
-            val repository = MovieRepositoryImpl(service)
-            return HomeViewModel(repository) as T
+//            val repository = MovieRepositoryImpl(service)
+            val useCase = TrailUseCaseImpl()
+            return HomeViewModel(useCase) as T
         }
     }
 
