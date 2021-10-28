2a8e53f823dbd1b6da7de8b39af0092c9692b2d4
João Pedro Limão <jplimao12@gmail.com>
2021-10-28 10:32:38 -0300

    Create UI from detail view

--- ui/src/main/java/com/example/ui/detail/MovieDetailFragment.kt
+++ ui/src/main/java/com/example/ui/detail/MovieDetailFragment.kt
@@ -1,10 +1,12 @@
 package com.example.ui.detail
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.example.ui.databinding.MovieDetailFragmentBinding
 import com.example.ui.detail.MovieDetailViewModelFactory.make
 
@@ -29,7 +31,11 @@ class MovieDetailFragment: Fragment(){
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         viewModel.movie.observe(viewLifecycleOwner) { movie ->
             with(bind!!) {
-                txtTitleMovie.text = movie.title
+                txtMovieTitle.text = movie.title
+                txtMovieDescription.text = movie.description
+                txtMovieGenres.text = "Ação"
+                val uri = Uri.parse(movie.imageUrl)
+                Glide.with(root.context).load(uri).into(ivMovie)
             }
         }
         super.onViewCreated(view, savedInstanceState)
