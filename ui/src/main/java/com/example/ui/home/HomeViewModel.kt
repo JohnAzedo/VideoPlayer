b93a1073edf36b992019d0f25b99237643125a07
João Pedro Limão <johnazedo07@gmail.com>
2022-06-23 18:16:52 -0300

    Create tests to check trail view model

--- ui/src/main/java/com/example/ui/home/HomeViewModel.kt
+++ ui/src/main/java/com/example/ui/home/HomeViewModel.kt
@@ -7,17 +7,20 @@ import androidx.lifecycle.viewModelScope
 import com.example.domain.usecases.GetMovieUseCase
 import com.example.domain.usecases.GetTrailsUseCase
 import com.example.model.Trail
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class HomeViewModel(
     private val trailUseCase: GetTrailsUseCase,
+    private val dispatcher: CoroutineDispatcher = Dispatchers.IO
 ): ViewModel() {
 
     private val _trails = MutableLiveData<List<Trail>>()
     val trails: LiveData<List<Trail>> get() = _trails
 
     fun getTrails() {
-        viewModelScope.launch {
+        viewModelScope.launch(dispatcher) {
             val listTrail = trailUseCase.getTrails()
             _trails.postValue(listTrail)
         }
---
+++ ui/src/test/java/com/example/ui/ViewModelStubs.kt
@@ -0,0 +1,10 @@
+package com.example.ui
+
+import com.example.model.Trail
+
+object ViewModelStubs {
+
+    val listOfTrails = listOf(
+        Trail(id = 1, title = "Action", movies = listOf())
+    )
+}
