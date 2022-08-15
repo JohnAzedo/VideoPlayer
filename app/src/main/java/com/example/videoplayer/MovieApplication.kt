6e49655899fc34acf8a267837436ba551408c96f
João Pedro Limão <johnazedo07@gmail.com>
2022-08-15 10:20:22 -0300

    [home] Change home fragment

--- app/src/main/java/com/example/videoplayer/MovieApplication.kt
+++ app/src/main/java/com/example/videoplayer/MovieApplication.kt
@@ -1,6 +1,7 @@
 package com.example.videoplayer
 
 import android.app.Application
+import com.example.home.di.homeModule
 import com.example.videoplayer.di.appModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
@@ -14,6 +15,7 @@ class MovieApplication: Application() {
             androidLogger()
             androidContext(this@MovieApplication)
             modules(appModule)
+            modules(homeModule)
         }
     }
 }
