981f1c7b71a22501049033737b74a431608c9fc2
João Pedro Limão <johnazedo07@gmail.com>
2022-08-23 11:22:05 -0300

    Change Response extensions functions

--- commons/src/main/java/com/example/commons/errors/NetworkErrors.kt
+++ commons/src/main/java/com/example/commons/errors/NetworkErrors.kt
@@ -1,9 +1,11 @@
 package com.example.commons.errors
 
+import okhttp3.ResponseBody
+
 open class NetworkErrors: Throwable(){
     data class InternalServerError(val code: Int = 500): NetworkErrors()
     data class GeneralError(
-        val errorBody: String? = null,
+        val errorBody: ResponseBody? = null,
         val code: Int
     ): NetworkErrors()
 }
--- commons/src/main/java/com/example/commons/network/Result.kt
+++ commons/src/main/java/com/example/commons/network/Result.kt
@@ -4,26 +4,10 @@ import com.example.commons.errors.NetworkErrors
 import retrofit2.Response
 
 
-//fun <A: Any> Response<A>.result() = flow {
-//    val body = body()
-//    val code = code()
-//    when {
-//        isSuccessful && body != null -> emit(body)
-//        isSuccessful && body == null -> throw NetworkErrors.InternalServerError()
-//        else -> throw NetworkErrors.GeneralError(code = code)
-//    }
-//}
-
 fun <A: Any> Response<A>.onError(lambda: (t: Throwable) -> Unit): Response<A> {
     val body = body()
-    if(!isSuccessful){
-        lambda(
-            when(body) {
-                null -> NetworkErrors.InternalServerError()
-                else -> NetworkErrors.GeneralError(code = code())
-            }
-        )
-    }
+    if(isSuccessful && body != null) lambda(NetworkErrors.InternalServerError())
+    if(!isSuccessful) lambda(NetworkErrors.GeneralError(code = code(), errorBody = errorBody()))
     return this
 }
 
@@ -32,3 +16,4 @@ suspend fun <A: Any> Response<A>.onSuccess(lambda: suspend (A) -> Unit): Respons
     if(isSuccessful && body != null) lambda(body)
     return this
 }
+
